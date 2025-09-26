84084ed7c3ac681473c50a9eaf739abf0d31cd3c
TheArtistWeb <teamtheartistweb@gmail.com>
2025-09-26 11:35:49 +0300

    edit

--- src/js/user.js
+++ src/js/user.js
@@ -5,18 +5,35 @@
  */
 import { 
     inputUsername,
-    inputEmailAddress
+    inputEmailAddress,
+    inputPassword,
+    inputConfirmPassword,
+    inputCheckTermsOfUse
 } from "./utils.js";
 
+/**
+ * Create new account
+ */
 const validatationCreateNewAccount = ($form) => {
     const $inputUsername = $form.querySelector("[data-field='username']");
     const $inputEmail = $form.querySelector("[data-field='email']");
     const $inputPassword = $form.querySelector("[data-field='password']");
     const $inputConfirmPassword = $form.querySelector("[data-field='confirmPassword']");
     const $checkTermsOfUse = $form.querySelector("[data-field='checkTermsOfUse']");
+    const $btnSubmit = $form.querySelector("button[type='submit']");
+    let getPasswordValue;
+
+    $btnSubmit.disabled = true;
 
     $inputUsername.addEventListener("input", () => inputUsername($inputUsername));
     $inputEmail.addEventListener("input", () => inputEmailAddress($inputEmail));
+    $inputPassword.addEventListener("input", (e) => {
+        getPasswordValue = e.target.value.trim();
+
+        inputPassword($inputPassword);
+    });
+    $inputConfirmPassword.addEventListener("input", () => inputConfirmPassword($inputConfirmPassword,  getPasswordValue));
+    $checkTermsOfUse.addEventListener("change", () => inputCheckTermsOfUse($checkTermsOfUse, $btnSubmit));
 };
 
 const submitCreateNewAccount = (e, $form) => {
@@ -30,6 +47,7 @@ const submitCreateNewAccount = (e, $form) => {
     const $inputPassword = $form.querySelector("[data-field='password']");
     const $inputConfirmPassword = $form.querySelector("[data-field='confirmPassword']");
     const $checkTermsOfUse = $form.querySelector("[data-field='checkTermsOfUse']");
+    const $btnSubmit = $form.querySelector("button[type='submit']");
 
     // functions
     if (!inputUsername($inputUsername)) 
@@ -37,6 +55,15 @@ const submitCreateNewAccount = (e, $form) => {
 
     if (!inputEmailAddress($inputEmail))
         allValid = false;
+
+    if (!inputPassword($inputPassword))
+            allValid = false;
+
+    if (!inputConfirmPassword($inputConfirmPassword, $inputPassword.value.trim()))
+            allValid = false;
+
+    if (!inputCheckTermsOfUse($checkTermsOfUse, $btnSubmit))
+            allValid = false;
 
     // reset input & validation
     if (allValid) {
@@ -48,6 +75,11 @@ const submitCreateNewAccount = (e, $form) => {
         $form.querySelectorAll(".validation").forEach(elem => elem.classList.remove("show"));
     }
 };
+
+/**
+ * Login
+ */
+
 
 export {
     submitCreateNewAccount,
--- src/js/utils.js
+++ src/js/utils.js
@@ -5,9 +5,7 @@ const dir = localStorage.getItem("dir") || document.documentElement.dir;
 /**
  * Add event on elements
  */
-const addEventOnElements = ($elements, eventType, callback) => {
-    $elements.forEach(elem => elem.addEventListener(eventType, callback));
-};
+const addEventOnElements = ($elements, eventType, callback) => $elements.forEach(elem => elem.addEventListener(eventType, callback));
 
 /**
  * Input username
@@ -25,8 +23,8 @@ const inputUsername = ($field) => {
     $validation.classList.remove("show", "error", "success", "warning");
 
     if (value === "") {
-        $field.classList.add("warning");
-        $validation.classList.add("show", "warning");
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
         $validation.innerHTML = dir === "ltr" ? "Username cannot be left blank." : "لا يمكن ترك اسم المستخدم فارغًا.";
 
         return false;
@@ -79,8 +77,8 @@ const inputEmailAddress = ($field) => {
 
     if (value === "") {
         $validation.innerHTML = dir === "ltr" ? "Email cannot be left blank" : "لا يمكن ترك الايميل فارغ";
-        $field.classList.add("warning");
-        $validation.classList.add("show", "warning");
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
 
         return false;
     } else if (value.startsWith("_") || value.startsWith("-")) {
@@ -116,8 +114,226 @@ const inputEmailAddress = ($field) => {
     }
 };
 
+/**
+ * Input apssword
+ */
+const inputPassword = ($field) => {
+    const value = $field.value.trim();
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    const $formBox = $field.closest(".form-box");
+    const $validation = $formBox.querySelector(".validation");
+
+    $validation.innerHTML = "";
+    $field.classList.remove("error", "warning", "success");
+    $validation.classList.remove("show", "error", "warning", "success");
+
+    if (value === "") {
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
+        $validation.innerHTML = dir === "ltr" ? "Password cannot be left blank." : "لا يمكن ترك كلمة المرور فارغًة.";
+
+        return false;
+    } else if (value.startsWith("_") || value.startsWith("-")) {
+        $field.classList.add("warning");
+        $validation.classList.add("show", "warning");
+        $validation.innerHTML = dir === "ltr" 
+            ? "Password cannot start with _ or -." 
+            : "لا يمكن أن تبدأ كلمة المرور بشرطة أو شرطة سفلية.";
+
+        return false;
+    } else if (value.endsWith("_") || value.endsWith("-")) {
+        $field.classList.add("warning");    
+        $validation.classList.add("show", "warning");
+        $validation.innerHTML = dir === "ltr" 
+            ? "Password cannot end with _ or -." 
+            : "لا يمكن أن تنتهي كلمة المرور '-' أو '_'.";
+
+        return false;
+    } else if (value !== value.split(" ").join("")) {
+        $field.classList.add("warning");
+        $validation.classList.add("show", "warning");
+        $validation.innerHTML = dir === "ltr" 
+            ? "Password cannot contain spaces." 
+            : "لا يمكن أن تحتوي كلمة المرور على مسافات.";
+
+        return false;
+    } else if (value.length <= 8) {
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
+        $validation.innerHTML = dir === "ltr" 
+            ? "Password must be longer than 8 characters." 
+            : "يجب أن تكون كلمة المرور أطول من 8 أحرف.";
+
+        return false;
+    } else if (value.length >= 30) {
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
+        $validation.innerHTML = dir === "ltr" 
+            ? "Password must be less than 30 characters." 
+            : "يجب أن تكون كلمة المرور أقل من 30 حرفًا.";
+
+        return false;
+    } else if (!regex.test(value)) {
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
+        $validation.innerHTML = dir === "ltr" 
+            ? "Password must contain uppercase, lowercase, number, and special character." 
+            : "يجب أن تحتوي كلمة المرور على حرف كبير وصغير ورقم ورمز خاص.";
+
+        return false;
+    } else {
+        $validation.innerHTML = dir === "ltr" ? "Password valid" : "كلمة المرور صالحة";
+        $field.classList.add("success");
+        $validation.classList.add("show", "success");
+
+        return true;
+    }
+}
+
+/**
+ * Input apssword
+ */
+const inputConfirmPassword = ($field, $passwordValue) => {
+    const value = $field.value.trim();
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    const $formBox = $field.closest(".form-box");
+    const $validation = $formBox.querySelector(".validation");
+
+    $validation.innerHTML = "";
+
+    $field.classList.remove("error", "warning", "success");
+
+    $validation.classList.remove("show", "error", "warning", "success");
+
+    if (value === "") {
+        $field.classList.add("error");
+
+        $validation.classList.add("show", "error");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password cannot be left blank." 
+            : "لا يمكن ترك تأكيد كلمة المرور فارغًا.";
+
+        return false;
+    } else if (value.startsWith("_") || value.startsWith("-")) {
+        $field.classList.add("warning");
+
+        $validation.classList.add("show", "warning");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password cannot start with _ or -." 
+            : "لا يمكن أن يبدأ تأكيد كلمة المرور بشرطة أو شرطة سفلية.";
+
+        return false;
+    } else if (value.endsWith("_") || value.endsWith("-")) {
+        $field.classList.add("warning");    
+
+        $validation.classList.add("show", "warning");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password cannot end with _ or -." 
+            : "لا يمكن أن ينتهي تأكيد كلمة المرور '-' أو '_'.";
+
+        return false;
+    } else if (value !== value.split(" ").join("")) {
+        $field.classList.add("warning");
+
+        $validation.classList.add("show", "warning");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password cannot contain spaces." 
+            : "لا يمكن أن يحتوي تأكيد كلمة المرور على مسافات.";
+
+        return false;
+    } else if (value.length <= 8) {
+        $field.classList.add("error");
+
+        $validation.classList.add("show", "error");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password must be longer than 8 characters." 
+            : "يجب أن يكون تأكيد كلمة المرور أطول من 8 أحرف.";
+
+        return false;
+    } else if (value.length >= 30) {
+        $field.classList.add("error");
+
+        $validation.classList.add("show", "error");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password must be less than 30 characters." 
+            : "يجب أن يكون تأكيد كلمة المرور أقل من 30 حرفًا.";
+
+        return false;
+    } else if (!regex.test(value)) {
+        $field.classList.add("error");
+
+        $validation.classList.add("show", "error");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Confirm password must contain uppercase, lowercase, number, and special character." 
+            : "يجب أن يحتوي تأكيد كلمة المرور على حرف كبير وصغير ورقم ورمز خاص.";
+
+        return false;
+    } else if ($passwordValue && value !== $passwordValue) {
+        $field.classList.add("error");
+
+        $validation.classList.add("show", "error");
+
+        $validation.innerHTML = dir === "ltr" 
+            ? "Passwords do not match." 
+            : "كلمة المرور غير متطابقة.";
+
+
+        return false;
+    } else {
+        $field.classList.add("success");
+        
+        $validation.classList.add("show", "success");
+        
+        $validation.innerHTML = dir === "ltr" ? "Confirm password valid" : "تأكيد كلمة المرور صالح";
+
+        return true;
+    }
+}
+
+/**
+ * Input check terms of use
+ */
+const inputCheckTermsOfUse = ($field, $btn) => {
+    const $formBox = $field.closest(".form-box");
+    const $validation = $formBox.querySelector(".validation");
+
+    $validation.innerHTML = "";
+    $field.classList.remove("error", "warning", "success");
+    $validation.classList.remove("show", "error", "warning", "success");
+
+    console.log($field.checked)
+    
+    if (!$field.checked) {
+        $field.classList.add("error");
+        $validation.classList.add("show", "error");
+        $validation.innerHTML = dir === "ltr" 
+            ? "You must accept the terms of use." 
+            : "يجب الموافقة على شروط الاستخدام.";
+
+        $btn.disabled = true;
+
+        return false;
+    } else {
+        $btn.disabled = false;
+
+        return true;
+    }
+}
+
 export {
     addEventOnElements,
     inputUsername,
-    inputEmailAddress
+    inputEmailAddress,
+    inputPassword,
+    inputConfirmPassword,
+    inputCheckTermsOfUse
 }
