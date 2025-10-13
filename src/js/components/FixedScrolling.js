a7a429e9e82b427a1884fa96aafdee62af55a8fc
TheArtistWeb <teamtheartistweb@gmail.com>
2025-10-13 08:50:44 +0300

    edit

--- src/js/components/FixedScrolling.js
+++ src/js/components/FixedScrolling.js
@@ -1,8 +1,1 @@
 "use strict";
-
-export const FixedScrolling = ($elem) => {
-    if (scrollY >= 50)
-        $elem.classList.add("active");
-    else
-        $elem.classList.remove("active");
-};
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,1 @@
 "use strict";
-
-/**
- * Add event on element
- */
-
-const addEventOnElements = ($elements, eventType, callback) => $elements.forEach($elem => $elem.addEventListener(eventType, (e) => callback(e, $elem)));
-
-export {
-    addEventOnElements
-}
