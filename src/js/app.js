a48beab7fae026ff9f6ffbe56a0cd24537f36db3
TheArtistWeb <teamtheartistweb@gmail.com>
2025-10-30 21:32:40 +0300

    edit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+/**
+ * Import modules
+ */
+import { StartLoading } from "./components/StartLoading.js";
+
+/**
+ * Get overlay & body
+ */
+const $overlay = document.querySelector("[data-overlay]");
+const $body = document.body;
+
+/**
+ * Start Loading
+ */
+const $startLoading = document.querySelector("[data-start-loading]");
+window.addEventListener("load", () => StartLoading($startLoading, $body));
