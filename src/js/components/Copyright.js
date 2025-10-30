660ac51e7216b78c6c438be1248071e3e902be53
TheArtistWeb <teamtheartistweb@gmail.com>
2025-10-30 13:24:28 +0300

    edit

--- src/js/components/Copyright.js
+++
@@ -1,7 +0,0 @@
-export const Copyright = ($element) => {
-    const date = new Date();
-    const dateYear = date.getFullYear();
-
-    $element.innerHTML = "";
-    $element.innerHTML = `Copyright Ⓒ ${dateYear} <strong>${$element.dataset.copyrights}</strong>, Inc.`;
-}
--- src/js/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const addEventOnElements = ($elements, eventType, callback) => $elements.forEach($elem => $elem.addEventListener(eventType, (e) => callback(e, $elem)));
-
-export {
-    addEventOnElements
-}
