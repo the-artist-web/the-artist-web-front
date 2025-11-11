df81ce1329659c87d0fa4b2ad3893b1a51721e40
TheArtistWeb <teamtheartistweb@gmail.com>
2025-11-11 12:42:45 +0200

    edit

--- src/js/components/StarRating.js
+++ src/js/components/StarRating.js
@@ -2,6 +2,21 @@
 
 export const StarRating = ($element) => {
     const $stars = $element.querySelectorAll("[data-star]");
+    const $starsSVG = $element.querySelectorAll("[data-star] svg");
 
-    $stars.forEach(($star, index1) => $star.addEventListener("click", () => $stars.forEach(($star, index2) => index1 >= index2 ? $star.classList.add("active") : $star.classList.remove("active"))));
+    $stars.forEach(($star, index1) => {
+        $star.addEventListener("click", () => {
+            $stars.forEach(($s, index2) => {
+                $s.classList.toggle("active", index2 <= index1);
+
+                $s.querySelector("svg").style.color = $s.classList.contains("active") ? "var(--primary)" : "var(--outline-variant)";
+            });
+        });
+    });
+
+    $starsSVG.forEach(($star, index1) => {
+        $star.addEventListener("mouseenter", () => $starsSVG.forEach(($s, index2) => $s.style.color = index2 <= index1 ? "var(--primary)" : "var(--outline-variant)"));
+
+        $star.addEventListener("mouseleave", () => $stars.forEach(($s) => $s.querySelector("svg").style.color = $s.classList.contains("active") ? "var(--primary)" : "var(--outline-variant)"));
+    });
 };
