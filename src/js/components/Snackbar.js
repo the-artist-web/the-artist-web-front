7a72558187731fbe9b95c93b5c1ce2e0f7c1607a
TheArtistWeb <teamtheartistweb@gmail.com>
2025-11-23 09:13:36 +0200

    edit

--- src/js/components/Snackbar.js
+++ src/js/components/Snackbar.js
@@ -2,13 +2,13 @@
 
 export const Snackbar = ($value) => {
     const $snackbar = document.createElement("div");
-    $snackbar.classList.add("snackbar");
+    $snackbar.classList.add("snackbar", "active");
 
-    $snackbar.remove();
+    document.querySelectorAll(".snackbar.active").forEach($elem => $elem.classList.remove("active"));
 
     $snackbar.innerHTML = `<p class="line-clamp line-clamp-1 text-capitalize text-body-large">${$value}</p>`;
 
     document.body.appendChild($snackbar);
 
-    setTimeout(() => $snackbar.remove(), 3500);
+    setTimeout(() => $snackbar.classList.remove("active"), 3500);
 };
