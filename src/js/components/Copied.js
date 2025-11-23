b6592ffd4241db18b5e4b203197768db9b454d56
TheArtistWeb <teamtheartistweb@gmail.com>
2025-11-23 19:59:01 +0200

    edit

--- src/js/components/Copied.js
+++ src/js/components/Copied.js
@@ -7,7 +7,7 @@ import { Snackbar } from "./Snackbar.js";
 
 export const Copied = ($element) => {
     $element.addEventListener("click", () => {
-        Snackbar("Copied!");
+        Snackbar(document.documentElement.dir === "ltr" ? "Copied!" : "تم نسخها!");
 
         navigator.clipboard.writeText($element.dataset.copied);
     });
