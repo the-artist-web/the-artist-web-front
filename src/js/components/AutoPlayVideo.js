746d2190d59471d2d7d8e0a40cb28ef8112c6158
TheArtistWeb <teamtheartistweb@gmail.com>
2025-12-01 14:37:49 +0200

    edit

--- src/js/components/AutoPlayVideo.js
+++ src/js/components/AutoPlayVideo.js
@@ -1,6 +1,16 @@
 "use strict";
 
 export const AutoPlayVideo = ($element) => {
+    const $videoControl = $element.closest("[data-videos-control]");
+    const $video = $videoControl.querySelector("[data-video]");
+    const $play = $videoControl.querySelector("[data-play]");
+    const $stop = $videoControl.querySelector("[data-stop]");
+    const $audio = $videoControl.querySelector("[data-audio]");
+    const $muted = $videoControl.querySelector("[data-muted]");
+
+    $element.muted = true;
+    $element.loop = true;
+
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
