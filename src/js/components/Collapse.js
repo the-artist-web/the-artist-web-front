6e7e74fe0c0ad0f7698611bd959a9d87bf65ad32
TheArtistWeb <teamtheartistweb@gmail.com>
2025-12-08 09:24:04 +0200

    edi

--- src/js/components/Collapse.js
+++ src/js/components/Collapse.js
@@ -1,17 +1,30 @@
 "use strict";
 
 export const Collapse = ($element) => {
-    const $collapseContent = document.querySelector(`${$element.dataset.togglerCollapses}`);
+    const $collapseContent = document.querySelector($element.dataset.togglerCollapses);
     const $arrow = $element.querySelector("[data-arrow]");
+
+    const updateHeights = ($content) => {
+        if ($content.classList.contains("show"))
+            $content.style.height = $content.scrollHeight + "px";
+        else
+            $content.style.height = "0px";
+
+        let parent = $content.parentElement.closest(".collapse-content");
+
+        while (parent) {
+            parent.style.height = parent.scrollHeight + "px";
+            parent = parent.parentElement.closest(".collapse-content");
+        }
+    };
 
     $element.addEventListener("click", () => {
         $element.classList.toggle("show");
         $collapseContent.classList.toggle("show");
         $arrow?.classList.toggle("active");
 
-        if ($collapseContent.classList.contains("show"))
-            $collapseContent.style.height = $collapseContent.scrollHeight + "px";
-        else
-            $collapseContent.style.height = "0px";
+        updateHeights($collapseContent);
     });
+
+    $collapseContent.addEventListener("transitionend", (e) => e.propertyName === "height" ? updateHeights($collapseContent) : "");
 };
