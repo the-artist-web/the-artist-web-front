a799a6cd561600ce08612c05d03f119bceac817d
TheArtistWeb <teamtheartistweb@gmail.com>
2025-12-17 12:54:54 +0200

    edit

--- src/js/components/Swipper.js
+++ src/js/components/Swipper.js
@@ -4,40 +4,33 @@ export const Swipper = ($element) => {
     const $swipperLeft = $element.querySelector("[data-swipper-left]");
     const $swipperRight = $element.querySelector("[data-swipper-right]");
     const $swipperContainer = $element.querySelector("[data-swipper-container]");
+    const dir = document.documentElement.dir;
 
     const amount = 500;
 
-    const updateButtons = () => {
-        const maxScroll = $swipperContainer.scrollWidth - $swipperContainer.clientWidth;
-
-        if ($swipperContainer.scrollLeft <= 0)
-            $swipperLeft.disabled = true;
-        else
-            $swipperLeft.disabled = false;
-
-        if ($swipperContainer.scrollLeft >= maxScroll)
-            $swipperRight.disabled = true;
-        else
-            $swipperRight.disabled = false;
-    };
-
     $swipperLeft.addEventListener("click", () => {
-        $swipperContainer.scrollBy({
-            left: -amount,
-            behavior: "smooth"
-        });
-
-        setTimeout(updateButtons, 300);
+        if (dir === "ltr")
+            $swipperContainer.scrollBy({
+                left: -amount,
+                behavior: "smooth"
+            });
+        else
+            $swipperContainer.scrollBy({
+                left: amount,
+                behavior: "smooth"
+            });
     });
 
     $swipperRight.addEventListener("click", () => {
-        $swipperContainer.scrollBy({
-            left: amount,
-            behavior: "smooth"
-        });
-
-        setTimeout(updateButtons, 300);
+        if (dir === "ltr")
+            $swipperContainer.scrollBy({
+                left: amount,
+                behavior: "smooth"
+            });
+        else
+            $swipperContainer.scrollBy({
+                left: -amount,
+                behavior: "smooth"
+            });
     });
-
-    updateButtons();
 };
