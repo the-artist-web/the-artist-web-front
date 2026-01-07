9a50de3ada825382eb3a6a6093fb35582f847b52
TheArtistWeb <teamtheartistweb@gmail.com>
2026-01-07 13:01:14 +0200

    edit

--- src/js/components/StartLoading.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-export const StartLoading = ($element, $body) => {
-    setTimeout(() => {
-        $element.classList.add("active");
-        $body.classList.add("load");
-    }, 800);
-};
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,1 @@
 "use strict";
-
-/**
- * Add Event On Elements
- */
-const addEventOnElements = ($elements, eventType, callback) => $elements.forEach($elem => $elem.addEventListener(eventType, (e) => callback(e, $elem)));
-
-export {
-    addEventOnElements
-}
