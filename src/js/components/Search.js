f52f60cccb753b435f066bf04c19229d7f6d5942
TheArtistWeb <teamtheartistweb@gmail.com>
2026-03-15 18:01:16 +0200

    edit

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -4,8 +4,12 @@ export const Search = ($element) => {
     const $inputSearch = $element.querySelector("[data-input-search]");
     const $searchContainer = $element.querySelector("[data-search-container]");
     const $headerItems = $element.querySelectorAll("[data-header-items]");
+    const $searchOverlay = $element.querySelector("[data-search-overlay]");
     const $searchBar = $element.querySelector("[data-searchbar]");
     const $openSearch = $element.querySelector("[data-open-search]");
+    const $searchFor = $element.querySelector("[data-search-for]");
+    const $recentSearches = $element.querySelector("[data-recent-searches]");
+    const $clearAllRecentSearches = $element.querySelector("[data-clear-all-recent-searches]");
 
     $openSearch.addEventListener("click", (e) => {
         e.stopPropagation()
@@ -15,13 +19,25 @@ export const Search = ($element) => {
         setTimeout(() => $inputSearch.focus(), 50);
     });
 
-    $inputSearch.addEventListener("focus", () => $searchBar.classList.add("active"));
+    $inputSearch.addEventListener("focus", () => {
+        if ($inputSearch.value.length > 0) {
+            $searchBar.classList.add("active");
+            $searchOverlay.classList.remove("active");
+        } else {
+            $searchOverlay.classList.add("active");
+            $searchBar.classList.remove("active");
+        }
+    });
 
     $inputSearch.addEventListener("input", (e) => {
-        if (e.target.value.length >= 1)
+        $searchFor.innerHTML = `“ ${e.target.value} “`
+
+        if (e.target.value.length >= 1) {
             $searchBar.classList.add("active");
-        else
+            $searchOverlay.classList.remove("active");
+        } else {
             $searchBar.classList.remove("active");
+        }
     });
     
     document.addEventListener("click", (e) => {
@@ -30,7 +46,10 @@ export const Search = ($element) => {
         if (isOutside) {
             $searchContainer.classList.remove("active");
             $searchBar.classList.remove("active");
+            $searchOverlay.classList.remove("active");
             $headerItems.forEach($elem => $elem.style.display = "");
         }
     });
+
+    $clearAllRecentSearches.addEventListener("click", () => $recentSearches.style.display = "none");
 };
