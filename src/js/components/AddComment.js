a5e1bfac971792a80e618901abefec5a4b277bce
TheArtistWeb <teamtheartistweb@gmail.com>
2026-04-11 20:44:50 +0200

    edit

--- src/js/components/AddComment.js
+++ src/js/components/AddComment.js
@@ -1,28 +1,47 @@
 "use strict";
 
 export const AddComment = ($element) => {
-    const $textarea = $element.querySelector("[data-textarea]");
+    const $inputChat = $element.querySelector(".input-chat");
     const $submit = $element.querySelector("button[type='submit']");
     const $emojies = $element.querySelectorAll("[data-emoji]");
-    
-    const updateTextarea = () => {
-        $submit.disabled = $textarea.value.trim().length === 0;
-        
-        $textarea.style.height = "48px";
-        $textarea.style.height = $textarea.scrollHeight + "px";
+
+    const updateButtonState = () => {
+        const text = $inputChat.innerText.trim();
+        $submit.disabled = text.length === 0;
     };
 
-    $textarea.addEventListener("input", updateTextarea);
+    $inputChat.addEventListener("input", () => {
+        if ($inputChat.innerHTML === "<br>") $inputChat.innerHTML = "";
+
+        updateButtonState();
+    });
 
     $emojies.forEach($elem => {
         $elem.addEventListener("click", () => {
             const emoji = $elem.innerText;
-            
-            $textarea.value += emoji;
-            
-            $textarea.focus();
 
-            updateTextarea();
+            insertAtCursor($inputChat, emoji);
+
+            updateButtonState();
+            $inputChat.focus();
         });
     });
+
+    function insertAtCursor(el, text) {
+        const selection = window.getSelection();
+
+        if (!selection.rangeCount) return;
+
+        const range = selection.getRangeAt(0);
+        range.deleteContents();
+
+        const textNode = document.createTextNode(text);
+        range.insertNode(textNode);
+
+        range.setStartAfter(textNode);
+        range.setEndAfter(textNode);
+
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
 };
