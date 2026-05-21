ebf1ac1befb43b9cf5271bde13272e684cd5c2d9
TheArtistWeb <teamtheartistweb@gmail.com>
2026-05-21 20:44:08 +0300

    edit

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,1 +1,44 @@
 "use strict";
+
+/**
+ * Import Modules
+ */
+import { addEventOnElements } from "./utils.js";
+
+/**
+ * Get DOM
+ */
+const $html = document.documentElement;
+const $btnsTheme = document.querySelectorAll("[data-btns-theme]");
+
+const getSystemTheme = () => {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+};
+
+const applyTheme = (theme) => {
+    let activeTheme = theme;
+
+    if (theme === "system") activeTheme = getSystemTheme();
+
+    $html.dataset.theme = activeTheme;
+
+    $btnsTheme.forEach($btn => {
+        if ($btn.dataset.btnsTheme === theme)
+            $btn.classList.add("active");
+        else
+            $btn.classList.remove("active");
+    });
+};
+
+addEventOnElements($btnsTheme, "click", function (e, $elem) {
+    const selectedTheme = $elem.dataset.btnsTheme;
+
+    localStorage.setItem("theme", selectedTheme);
+
+    applyTheme(selectedTheme);
+});
+
+const savedTheme = localStorage.getItem("theme") || "system";
+applyTheme(savedTheme);
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => localStorage.getItem("theme") === "system" ? applyTheme("system") : "");
